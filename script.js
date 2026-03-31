f7d11cef75b9097dd9efa5437c2db5b76e82dced
Octávio Mello Covre de Souza <octaviomellocovredesouza@gmail.com>
2026-03-31 20:09:13 +0100

    Organizando código

--- script.js
+++ script.js
@@ -8,11 +8,12 @@ function gerarNumeroSecreto(){
     return parseInt((Math.random() * numeroMaximo) + 1);
 }
 
-let numeroSecreto = gerarNumeroSecreto()
+let numeroSecreto = gerarNumeroSecreto();
+console.log(numeroSecreto);
 
 function exibirTextoDeBaixo(id, texto){
     let mensagem = document.getElementById(id);
-    mensagem.innerHTML = texto
+    mensagem.innerHTML = texto;
 }
 
 function exibirTextoDeCima(id, texto){
@@ -25,28 +26,56 @@ function limparCampo(){
     document.getElementById('inputNumero'). value = '';
 }
 
+function botoesFim(){
+    document.getElementById('btnChutar').setAttribute('disabled', true);
+    document.getElementById('btnReiniciar').removeAttribute('disabled');
+}
+
+function botoesInicio(){
+    document.getElementById('btnReiniciar').setAttribute('disabled', true);
+    document.getElementById('btnChutar').removeAttribute('disabled');
+}
+
 function reiniciarJogo(){
     numeroSecreto = gerarNumeroSecreto();
     exibirTextoDeCima('textoCima', `Escolha um número entre 1 e ${numeroMaximo}`);
     exibirTextoDeBaixo('mensagem', '');
     limparCampo();
     tentativas = 1;
-    document.getElementById('btnReiniciar').setAttribute('disabled', true);
-    document.getElementById('btnChutar').removeAttribute('disabled')
+    botoesInicio();
     numerosChutados = [];
 }
+
 function limiteTentativas(){
     if (tentativas > maximoTentativas){
-        exibirTextoDeCima('textoCima', `Você atingiu o limite de tentativas.<br>O número secreto era ${numeroSecreto}`);
-        exibirTextoDeBaixo('mensagem', "Tente novamente em 'reiniciar'.");
-        document.getElementById('btnChutar').setAttribute('disabled', true);
-        document.getElementById('btnReiniciar').removeAttribute('disabled');
+        exibirTextoDeCima('textoCima', `Você atingiu o limite de tentativas.<br>O número secreto era ${numeroSecreto}.`);
+        exibirTextoDeBaixo('mensagem', "Tente novamente em 'Reiniciar'.");
+        botoesFim();
     }
+}
+
+function verificarValor(numerosChutados, chute, numeroSecreto){
+        numerosChutados.push(chute);
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        if (chute == numeroSecreto){
+            botoesFim();
+            exibirTextoDeCima('textoCima', `Parabéns, você adivinhou o número secreto em ${tentativas} ${palavraTentativa}!<br>Os números chutados foram: ${numerosChutados}.`);
+            exibirTextoDeBaixo('mensagem', 'Aperte "Reiniciar" para jogar novamente.');
+        }
+        else{
+            tentativas++;
+            limparCampo();
+            if(chute > numeroSecreto){
+                exibirTextoDeBaixo('mensagem', `Você errou! O número secreto é menor que ${b}`);
+            }
+            else{
+               exibirTextoDeBaixo('mensagem', `Você errou! O número secreto é maior que ${b}`);
+            }
+        }
 }
 
 function verificarChute(){
     let chute = parseInt(document.getElementById('inputNumero').value);
-    let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
     if (chute <1 || chute > numeroMaximo || isNaN(chute)){
         exibirTextoDeBaixo('mensagem', 'Valor inválido.');
         limparCampo();
@@ -56,23 +85,7 @@ function verificarChute(){
         limparCampo();
     }
     else{
-        numerosChutados.push(chute);
-        if (chute == numeroSecreto){
-            document.getElementById('btnReiniciar').removeAttribute('disabled');
-            document.getElementById('btnChutar').setAttribute('disabled', true);
-            exibirTextoDeCima('textoCima', `Parabéns, você adivinhou o número secreto em ${tentativas} ${palavraTentativa}!<br>Os números chutados foram: ${numerosChutados}.`);
-            exibirTextoDeBaixo('mensagem', 'Aperte "Reiniciar" para jogar novamente.');
-        }
-        else{
-            tentativas++;
-            limparCampo();
-            if(chute > numeroSecreto){
-                exibirTextoDeBaixo('mensagem', `Você errou! O número secreto é menor que ${chute}`)
-            }
-            else{
-               exibirTextoDeBaixo('mensagem', `Você errou! O número secreto é maior que ${chute}`) 
-            }
-        }
+        verificarValor(numerosChutados, chute, numeroSecreto);
     }
     limiteTentativas();
 }
